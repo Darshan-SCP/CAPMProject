c4e6f8c3bcdd3b1c5a74775890b6fc531dd32464
DARSHANKUMAR LAD <darsh3799@me.com>
2023-10-02 09:20:50 +0000

    main

---
+++ gen/srv/srv/ZIVN_VENDOR_REG_SRV.js
@@ -0,0 +1,13 @@
+const cds = require('@sap/cds');
+
+module.exports = async (srv) => 
+{        
+    // Using CDS API      
+    const ZIVN_VENDOR_REG_SRV = await cds.connect.to("ZIVN_VENDOR_REG_SRV"); 
+      srv.on('READ', 'BPTypeSet', req => ZIVN_VENDOR_REG_SRV.run(req.query)); 
+      srv.on('READ', 'CheckNewVendorCodeSet', req => ZIVN_VENDOR_REG_SRV.run(req.query)); 
+      srv.on('READ', 'CheckUpdateReqSet', req => ZIVN_VENDOR_REG_SRV.run(req.query)); 
+      srv.on('READ', 'GetStateSet', req => ZIVN_VENDOR_REG_SRV.run(req.query)); 
+      srv.on('READ', 'GetSortKeySet', req => ZIVN_VENDOR_REG_SRV.run(req.query)); 
+      srv.on('READ', 'GetVendorsSet', req => ZIVN_VENDOR_REG_SRV.run(req.query)); 
+}
