5351206aa187bb91170461a4d212481b52681e44
DARSHANKUMAR LAD <darsh3799@me.com>
2023-10-20 19:00:24 +0000

    main

--- srv/vendor_srv.js
+++ srv/vendor_srv.js
@@ -15,11 +15,23 @@ module.exports = cds.service.impl(function () {
         let S4Conn = await cds.connect.to('ZIVN_VENDOR_REG_SRV');
         // ZIVN_VENDOR_REG_SRV/GetCitySet
         let Payload = req.data;
-        const citydata = await S4Conn.send('POST', '/GetCitySet', Payload, { "Content-Type": "application/json" });
-        // console.log(citydata);
-        var outRes = citydata;
+        // const citydata = await S4Conn.send('POST', '/GetCitySet', Payload, { "Content-Type": "application/json" });
+        // // console.log(citydata);
+        // var outRes = citydata;
 
-        return outRes;
+        let oImportedCaseDetails =  await S4Conn.send({
+            method: 'POST',
+            path: '/$batch',
+            headers: { 'Content-Type': 'multipart/mixed;boundary=batch'},
+            data:  '--batch\r\n'+
+            'Content-Type: application/http\r\n'+
+            'Content-Transfer-Encoding: binary\r\n'+
+            '\r\n'+
+            'GET GetCitySet HTTP/1.1\r\n'+
+            '\r\n'+
+            '\r\n'+
+            '--batch--', });
+        return oImportedCaseDetails;
 
     });
 
